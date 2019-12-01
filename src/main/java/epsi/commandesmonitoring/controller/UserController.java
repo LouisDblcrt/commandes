5d0645393d0fbd437dbb203e90159f6c8da1f961
Lucas FRESIERS <fresiers.lucas@gmail.com>
2019-12-01 16:18:36 +0100

    Login ok, manque les redirections de pages

---
+++ src/main/java/epsi/commandesmonitoring/controller/UserController.java
@@ -0,0 +1,33 @@
+package epsi.commandesmonitoring.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import epsi.commandesmonitoring.model.User;
+import epsi.commandesmonitoring.services.UserService;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+}
